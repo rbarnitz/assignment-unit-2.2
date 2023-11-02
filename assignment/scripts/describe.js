2e8e6bbe20f2ad0ef01c5baf02a4d540336881c1
Richard Barnitz <richardbarnitz@gmail.com>
2023-11-02 11:01:46 -0700

    finished descriptions

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create a variable called name, and assign it a value of 'Dane'
+// We create a conditional if statement, comparing the value and type of name to the string/value 'Mary'
+// If the value and type of name are equal to 'Mary', the statement will output 'Hi, Mary'
+// If it does not, it will output 'How do you do?'
 
 //CODE
 /*
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We created a variable called secret, and a variable called code, to which we assigned the value 123
+// We created a conditional if statement comparing the variable 'code' to the value and type of 123
+// Since the value of code is 123, the variable secret is assigned the string 'super' and the value of 'code' is reassigned its priginal value multiplied by two by two
+// The next if statement checks if the value of 'code' is less than the value 250. Since it is not greater than 250,
+// the program moves to the console.log, and returns the value 'super'
 
 //CODE
 /*
@@ -70,10 +71,11 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We create three variables, and assign them three values
+// the first if statement asks if two conditions are met, that isStudent equals true, and 'zip' is greater than 80000. Only one of these is true. The code moves to the next conditional.
+// the second statement compares if either of the two conditionals are true, neither of which are. The code moves to the next conditional.
+// the third statement compares if isStuent is equal to true, which it is. It then console.logs the value 'Welcome to Prime!'
+// Once this condition is satisfied, the code moves to the end of the brackets and terminates.
 
 //CODE
 /*
@@ -88,7 +90,7 @@ if (isStudent === true && zip > 80000 ) {
 } else if (isStudent === true) {
   console.log('Welcome to Prime!');
 } else {
-  console.log('How about the weather?')
+  console.log('How about the weather?');
 }
 
 */
@@ -130,10 +132,15 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+
+//FIX - the values colorOne and colorBlue are assigned to incorrect variables
+// Assign correct values to these variables
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
+// this statement should assign both colorOne and colortwo to 'purple', it only assigns colorOne to purple
+// add: colorTwo = 'purple';
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -147,6 +154,8 @@ if (mix === true) {
 let temp = 40;
 const time = 4;
 
+//FIX - This conditional || is asking if either of the two conditions are met, the prompt asks that both conditions be met
+// SHould use && instead of ||
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -157,6 +166,8 @@ if (temp > 39 || time >= 4) {
 // -- it is, so we console.log 'enter'.
 
 /*
+//FIX - The prompt asks if age is greater than or equal to minAge, the conditional is comparing if minAge is less than or equal to age. 
+// change the conditional to (age >=minAge)
 let age = 21;
 const minAge = 21;
 
